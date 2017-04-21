5f61aa772f7d785997cdbbd4ef3b394b4df23a86
KainyGuo <gt@kainy.cn>
2017-04-21 11:52:29 +0800

    定制版。

--- dev/page/line-multi-axis/getConfig.js
+++ dev/page/line-multi-axis/getConfig.js
@@ -3,7 +3,7 @@
  */
 export default function(canvasConfig,labels,data){
     var chartColors = {
-        red: 'rgb(255, 99, 132)',
+        red: 'pink',
         orange: 'rgb(255, 159, 64)',
         yellow: 'rgb(255, 205, 86)',
         green: 'rgb(75, 192, 192)',
@@ -13,16 +13,18 @@ export default function(canvasConfig,labels,data){
     };
 
     var randomScalingFactor = function () {
-        return (Math.random() > 0.5 ? 1.0 : -1.0) * Math.round(Math.random() * 100);
+        // return (Math.random() > 0.5 ? 1.0 : -1.0) * Math.round(Math.random() * 100);
+        return Math.random() * 9
     }
     var MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var lineChartData = {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        labels: ["1", "2", "3", "4", "5", "6", "7"],
         datasets: [{
-            label: "My First dataset",
-            borderColor: chartColors.red,
-            backgroundColor: chartColors.red,
+            label: "小",
+            borderColor: chartColors.blue,
+            backgroundColor: chartColors.blue,
             fill: false,
+            spanGaps: true,
             data: [
                 randomScalingFactor(),
                 randomScalingFactor(),
@@ -32,22 +34,22 @@ export default function(canvasConfig,labels,data){
                 randomScalingFactor(),
                 randomScalingFactor()
             ],
-            yAxisID: "y-axis-1",
+            yAxisID: "y-axis-pee",
         }, {
-            label: "My Second dataset",
-            borderColor: chartColors.blue,
-            backgroundColor: chartColors.blue,
+            label: "大",
+            borderColor: chartColors.red,
+            backgroundColor: chartColors.red,
             fill: false,
             data: [
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                3000000000
+                2.9,
+                4,
+                3.2,
+                3.234,
+                2.43,
+                2.34,
+                3.9
             ],
-            yAxisID: "y-axis-2"
+            yAxisID: "y-axis-poo"
         }]
     };
     var chartConfig = {
@@ -59,25 +61,52 @@ export default function(canvasConfig,labels,data){
             stacked: false,
             title:{
                 display: true,
-                text:'Chart.js Line Chart - Multi Axis'
+                text:'大小便情况'
             },
             scales: {
                 yAxes: [{
                     type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
                     display: true,
                     position: "left",
-                    id: "y-axis-1",
+                    id: "y-axis-poo",
+                    ticks: {
+                        max: 9,
+                        min: 0,
+                        stepSize: 1,
+                        fontColor: 'pink',
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: '次数',
+                        fontColor: 'pink',
+                        fontSize: '10'
+                    }
                 }, {
                     type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
-                    display: true,
+                    display: false,
                     position: "right",
-                    id: "y-axis-2",
+                    id: "y-axis-pee",
 
                     // grid line settings
                     gridLines: {
                         drawOnChartArea: false, // only want the grid lines for one axis to show up
                     },
                 }],
+                xAxes: [{
+                    display: true,
+                    gridLines: {
+                        display: false
+                    },
+                    ticks: {
+                        fontColor: 'pink',
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: '日期',
+                        fontColor: 'pink',
+                        fontSize: '10'
+                    }
+                }]
             }
         }
     };
